9c6618df3177396fa74935b6076e46824652cfed
christian <christianbonnalie@gmail.com>
2025-05-25 15:11:42 -0700

    providerWrapper fix

--- src/app/ProviderWrapper.tsx
+++ src/app/ProviderWrapper.tsx
@@ -12,22 +12,25 @@ import Locales from 'components/Locales';
 import ScrollTop from 'components/ScrollTop';
 import Snackbar from 'components/@extended/Snackbar';
 import Notistack from 'components/third-party/Notistack';
+import { ConfigProvider } from '../contexts/ConfigContext';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL ||============================== //
 
 export default function ProviderWrapper({ children }: { children: ReactElement }) {
   return (
-    <ThemeCustomization>
-      <Locales>
-        <ScrollTop>
-          <SessionProvider refetchInterval={0}>
-            <Notistack>
-              <Snackbar />
-              {children}
-            </Notistack>
-          </SessionProvider>
-        </ScrollTop>
-      </Locales>
-    </ThemeCustomization>
+    <ConfigProvider>
+      <ThemeCustomization>
+        <Locales>
+          <ScrollTop>
+            <SessionProvider refetchInterval={0}>
+              <Notistack>
+                <Snackbar />
+                {children}
+              </Notistack>
+            </SessionProvider>
+          </ScrollTop>
+        </Locales>
+      </ThemeCustomization>
+    </ConfigProvider>
   );
 }
