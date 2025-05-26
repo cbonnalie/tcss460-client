d1a6ba2ff62453a3e6eb0591d1ce0259bf1f2605
christian <christianbonnalie@gmail.com>
2025-05-25 18:15:12 -0700

    remove authfooter import

--- src/themes/emotionCache.tsx
+++ src/themes/emotionCache.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Provider, ReactNode, useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 
 // next
 import { useServerInsertedHTML } from 'next/navigation';
@@ -14,7 +14,7 @@ export type NextAppDirEmotionCacheProviderProps = {
   /** This is the options passed to createCache() from 'import createCache from "@emotion/cache"' */
   options: Omit<OptionsOfCreateCache, 'insertionPoint'>;
   /** By default <CacheProvider /> from 'import { CacheProvider } from "@emotion/react"' */
-  CacheProvider?: Provider<EmotionCache>;
+  CacheProvider?: React.ComponentType<{ value: EmotionCache; children: ReactNode }>;
   children: ReactNode;
 };
 
