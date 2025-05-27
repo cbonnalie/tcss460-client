a6a0fe29a70c1b1055afa73af75bc9dbe5168f91
christian <christianbonnalie@gmail.com>
2025-05-27 15:04:23 -0700

    pipe null?

--- src/utils/transformBook.ts
+++ src/utils/transformBook.ts
@@ -1,16 +1,16 @@
-﻿export const transformBookData = (bookData) => ({
-  isbn13: bookData.isbn13,
-  authors: bookData.authors.split(', '),
-  publication: bookData.publication,
-  original_title: bookData.original_title,
-  title: bookData.title,
-  average: bookData.ratings.average,
-  count: bookData.ratings.count,
-  rating_1: bookData.ratings.rating_1,
-  rating_2: bookData.ratings.rating_2,
-  rating_3: bookData.ratings.rating_3,
-  rating_4: bookData.ratings.rating_4,
-  rating_5: bookData.ratings.rating_5,
-  large: bookData.icons.large,
-  small: bookData.icons.small
-});
+﻿// export const transformBookData = (bookData: any) => ({
+//   isbn13: bookData.isbn13,
+//   authors: bookData.authors.split(', '),
+//   publication: bookData.publication,
+//   original_title: bookData.original_title,
+//   title: bookData.title,
+//   average: bookData.ratings.average,
+//   count: bookData.ratings.count,
+//   rating_1: bookData.ratings.rating_1,
+//   rating_2: bookData.ratings.rating_2,
+//   rating_3: bookData.ratings.rating_3,
+//   rating_4: bookData.ratings.rating_4,
+//   rating_5: bookData.ratings.rating_5,
+//   large: bookData.icons.large,
+//   small: bookData.icons.small
+// });
